d59be47b3d7995eb53bbfcf56b64a5139537ae39
elmananaconsejo-byte <elmananaconsejo@gmail.com>
2026-04-20 19:02:07 -0500

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@
 
 // 1) URL de la Web App de Apps Script (termina en /exec)
 //    La obtienes al hacer "Implementar > Nueva implementación" en Apps Script.
-window.API_URL = 'https://script.google.com/macros/s/AKfycbxG-Sm2tfLvLGTcn0RfmS13lGDNCBEYQK5bySQTEUH0lyVRMW6z9VA193VeRH5yzMrI/exec';
+window.API_URL = 'https://script.google.com/macros/s/AKfycbwUHDhIaK7fC38F5jaDxbq-L5s9dxrhWHQC0uP8LXMZnVg3ja5Ac3eUqXe1GYTIB7_J/exec';
 
 // 2) Client ID de OAuth 2.0 (tipo "Aplicación web") desde Google Cloud Console.
 //    Crea uno en https://console.cloud.google.com/apis/credentials
